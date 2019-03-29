1e540c0c3319fb863c2fdef622e3685555d3bcf9
minhdien1710 <nguyenminhdien996@gmail.com>
2019-03-29 10:12:44 +0700

    add

---
+++ sdtoantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputHeight = prompt("Nhap chieu rong: ");
+inputWidth = prompt(" Nhap chieu cao: " );
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
